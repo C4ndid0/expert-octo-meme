163c59529f6a44e440a152f1bd093603e830f2bf
Lucas Candido <avnserver@gmail.com>
2024-05-04 00:37:14 -0300

    First Commit

---
+++ zero/src/test/java/br/com/candido/zero/ZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.candido.zero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
