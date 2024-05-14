bb67fdba95d01f240599ad06774b59b1b3d1da71
Lucas Candido <avnserver@gmail.com>
2024-05-14 03:37:42 -0300

    Add ResourceEntity

--- zero/src/main/java/br/com/candido/zero/entity/ResourceEntity.java
+++ zero/src/main/java/br/com/candido/zero/entity/ResourceEntity.java
@@ -1,6 +1,8 @@
 package br.com.candido.zero.entity;
 
+import br.com.candido.zero.dto.ResourceDTO;
 import jakarta.persistence.*;
+import org.springframework.beans.BeanUtils;
 
 import java.util.Objects;
 
@@ -10,33 +12,32 @@ public class ResourceEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     @Column(nullable = false)
     private String name;
-
     @Column(nullable = false)
     private String key;
+
+    public ResourceEntity(){}
+
+    public ResourceEntity(ResourceDTO resourceDTO) {
+        BeanUtils.copyProperties(resourceDTO, this);
+    }
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getKey() {
         return key;
     }
-
     public void setKey(String key) {
         this.key = key;
     }
