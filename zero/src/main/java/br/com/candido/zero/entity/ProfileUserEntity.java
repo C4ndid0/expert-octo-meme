95bed9a4628c3a1ce1d081e61f8a9e32fb5cc42f
Lucas Candido <avnserver@gmail.com>
2024-05-17 05:06:25 -0300

    add profile_user_entity

---
+++ zero/src/main/java/br/com/candido/zero/entity/ProfileUserEntity.java
@@ -0,0 +1,29 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "ZERO_PROFILE_USER_")
+@Getter
+@Setter
+@NoArgsConstructor
+public class ProfileUserEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "ID_USER")
+  private UserEntity user;
+  @ManyToOne
+  @JoinColumn(name = "ID_PROFILE")
+  private ProfileEntity profile;
+}
